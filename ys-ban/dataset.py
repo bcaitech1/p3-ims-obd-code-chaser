991c82166b08df430badb47afe1705ab38c31e26
ys-ban <bys940207@gmail.com>
2021-04-28 17:17:05 +0900

    ys updated by 28th April

---
+++ ys-ban/dataset.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset
+import cv2
+
+import os
+import numpy as np
+
+# 전처리를 위한 라이브러리
+from pycocotools.coco import COCO
+
+
+class CustomDataLoader(Dataset):
+    """COCO format"""
+    def __init__(self, dataset_path, data_dir, mode = 'train', transform = None):
+        super().__init__()
+        self.mode = mode
+        self.transform = transform
+        self.coco = COCO(data_dir)
+        self.dataset_path = dataset_path
+        
+    def __getitem__(self, index: int):
+        # dataset이 index되어 list처럼 동작
+        image_id = self.coco.getImgIds(imgIds=index)
+        image_infos = self.coco.loadImgs(image_id)[0]
+        
+        # cv2 를 활용하여 image 불러오기
+        images = cv2.imread(os.path.join(self.dataset_path, image_infos['file_name']))
+        images = cv2.cvtColor(images, cv2.COLOR_BGR2RGB).astype(np.float32)
+        images /= 255.0
+        
+        if (self.mode in ('train', 'val')):
+            ann_ids = self.coco.getAnnIds(imgIds=image_infos['id'])
+            anns = self.coco.loadAnns(ann_ids)
+
+            # masks : size가 (height x width)인 2D
+            # 각각의 pixel 값에는 "1" 할당
+            # Background = 0
+            masks = np.zeros((11, image_infos["height"], image_infos["width"]))
+            # Unknown = 0, General trash = 1, ... , Cigarette = 10
+            for i in range(len(anns)):
+                id_val = anns[i]['category_id']
+                masks[id_val] = np.maximum(self.coco.annToMask(anns[i]), masks[id_val])
+            masks = masks.astype(np.float32)
+
+            # transform -> albumentations 라이브러리 활용
+            if self.transform is not None:
+                transformed = self.transform(image=images, mask=masks)
+                images = transformed["image"]
+                masks = transformed["mask"]
+            
+            return images, masks#, image_infos
+        
+        if self.mode == 'test':
+            # transform -> albumentations 라이브러리 활용
+            if self.transform is not None:
+                transformed = self.transform(image=images)
+                images = transformed["image"]
+            image_infos['date_captured'] = 0
+            image_infos['url'] = 0
+            return images, image_infos
+    
+    
+    def __len__(self) -> int:
+        # 전체 dataset의 size를 return
+        return len(self.coco.getImgIds())
